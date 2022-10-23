ef6e6d867a0bf6a156f1668fc87ad1f13a1c672e
JerryGJX <1422765817@qq.com>
2022-10-23 17:05:24 +0800

    [semantic] first oj version

--- src/Compiler.java
+++ src/Compiler.java
@@ -19,8 +19,9 @@ import java.io.PrintStream;
 
 public class Compiler {
     public static void main(String[] args) throws Exception {
-        String fileName = "D:\\Work\\studying materials\\computer related\\compiler_design\\myCompiler\\Mx-Compiler\\src\\test\\testCase\\sema\\array-package\\array-7.mx";
-        InputStream inputStream = new FileInputStream(fileName);
+//        String fileName = "D:\\Work\\studying materials\\computer related\\compiler_design\\myCompiler\\Mx-Compiler\\src\\test\\testCase\\sema\\array-package\\array-7.mx";
+//        InputStream inputStream = new FileInputStream(fileName);
+        CharStream inputStream = CharStreams.fromStream(System.in);
         PrintStream outputStream = System.out;
 
         Log log = new Log();
@@ -29,7 +30,7 @@ public class Compiler {
 
             RootNode ASTRoot;
 
-            MxLexer lexer = new MxLexer(CharStreams.fromStream(inputStream));
+            MxLexer lexer = new MxLexer(inputStream);
             lexer.removeErrorListeners();
             lexer.addErrorListener(new MxErrorListener());
             MxParser parser = new MxParser(new CommonTokenStream(lexer));
@@ -40,10 +41,10 @@ public class Compiler {
             ASTRoot = (RootNode) astBuilder.visit(parseTreeRoot);
             new SymbolCollector(globalScope,log).visit(ASTRoot);
             new SemanticChecker(globalScope,log).visit(ASTRoot);
-            log.printLog();
+//            log.printLog();
         } catch (error er) {
-            log.printLog();
-            System.err.println(er.toString());
+//            log.printLog();
+//            System.err.println(er.toString());
             throw new RuntimeException();
         }
     }
